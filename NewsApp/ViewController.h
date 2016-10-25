9fe17c8a553faa782ca22965c7ba2979c03a1728
Andrew Soroka <andrewsoroka19@gmail.com>
2016-10-25 14:54:40 +0300

    Initial Commit

---
+++ NewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsApp
+//
+//  Created by Andrew Soroka on 10/25/16.
+//  Copyright © 2016 Andrew Soroka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
